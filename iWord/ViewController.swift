649b3531a3f8ac1f1e72b8f7748bfd1aa122ea52
Dennis <dnsljnv411@gmail.com>
2022-11-28 21:15:31 +0700

    Initial Commit

---
+++ iWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iWord
+//
+//  Created by Dennis Programmer on 28/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
