228ceea2ffd421e29a1bce65c45c50df85badf80
Dennis <dnsljnv411@gmail.com>
2022-11-29 20:41:52 +0700

    Add license headers

--- iWord/SupportingFiles/AppDelegate.swift
+++ iWord/SupportingFiles/AppDelegate.swift
@@ -1,9 +1,15 @@
+// Data of creation: 29/11/22
+// Create by Denis Ulianov aka Денис Александрович Ульянов
+// Using Swift 5.0
+// Running on macOS 12.5
 //
-//  AppDelegate.swift
-//  iWord
+// Copyrights
 //
-//  Created by Dennis Programmer on 28/11/22.
-//
+// I do not allow any person to use this piece of software
+// for any purposes.
+// This project and source code may use libraries or frameworks that are
+// released under various Open-Source licenses. Use of those libraries and
+// frameworks are governed by their own individual licenses.
 
 import UIKit
 
--- iWord/SupportingFiles/SceneDelegate.swift
+++ iWord/SupportingFiles/SceneDelegate.swift
@@ -1,3 +1,16 @@
+// Data of creation: 29/11/22
+// Create by Denis Ulianov aka Денис Александрович Ульянов
+// Using Swift 5.0
+// Running on macOS 12.5
+//
+// Copyrights
+//
+// I do not allow any person to use this piece of software
+// for any purposes.
+// This project and source code may use libraries or frameworks that are
+// released under various Open-Source licenses. Use of those libraries and
+// frameworks are governed by their own individual licenses.
+
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
