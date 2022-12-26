da0fe5937683a64a5042fc41ef0bbc1535fd1db3
Dennis <dnsljnv411@gmail.com>
2022-12-26 14:00:40 +0700

    Add LexicalUnitTableViewCell file

---
+++ iWord/Screens/LexicalUnitListScreen/Subviews/LexicalUnitTableViewCell.swift
@@ -0,0 +1,29 @@
+// Data of creation: 26/12/22
+// Create by Denis Ulianov aka Денис Александрович Ульянов
+// Using Swift 5.0
+// Running on macOS 12.5
+//
+// Copyrights
+//
+// I do not allow any person to use this piece of software
+// for any purposes. 
+// This project and source code may use libraries or frameworks that are
+// released under various Open-Source licenses. Use of those libraries and
+// frameworks are governed by their own individual licenses.
+
+import UIKit
+
+class LexicalUnitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
