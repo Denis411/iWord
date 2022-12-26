1818306302b4baf4af4be06130c690009dba60a1
Dennis <dnsljnv411@gmail.com>
2022-12-26 21:48:04 +0700

    Create cell for lexical units

--- iWord/Screens/LexicalUnitListScreen/Subviews/LexicalUnitTableViewCell.swift
+++ iWord/Screens/LexicalUnitListScreen/Subviews/LexicalUnitTableViewCell.swift
@@ -12,18 +12,118 @@
 // frameworks are governed by their own individual licenses.
 
 import UIKit
+import SnapKit
+
+struct LexicalUnitCellInfo {
+    let image: UIImage?
+    let progressPercentage: Int8
+    let unitOriginalValue: String
+    let unitPrimaryTranslation: String
+}
+
+fileprivate let UNIT_IMAGE_VIEW_EDGES: CGFloat = 20.0
+fileprivate let UNIT_IMAGE_LEFT_INSET: CGFloat = 20.0
+fileprivate let PROGRESS_VIEW_EDGE: CGFloat = 40.0
+fileprivate let PROGRESS_VIEW_INSET: CGFloat = 20.0
+fileprivate let STACK_VIEW_MERGIN: CGFloat = 10.0
+fileprivate let SPACING_BETWEEN_LABELS: CGFloat = 10.0
 
 class LexicalUnitTableViewCell: UITableViewCell {
+    static let reusableID = "LexicalUnitTableViewCell"
+    private let unitImageView = UIImageView()
+    private let labelStackView = UIStackView()
+    private let unitOriginalValue = UILabel()
+    private let unitPrimaryTranslation = UILabel()
+    //  TODO: progressView will be a colorful UIView after A B testing
+    private let progressView = UILabel()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpUI()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
+    public func setUpCellData(with model: LexicalUnitCellInfo) {
+        self.unitImageView.image = model.image
+        self.progressView.text = String(model.progressPercentage)
+        self.unitPrimaryTranslation.text = model.unitPrimaryTranslation
+        self.unitOriginalValue.text = model.unitOriginalValue
+    }
+}
+
+// set subviews
+extension LexicalUnitTableViewCell {
+    private func setUpUI() {
+        addSubviews()
+        setAllConstraints()
+        configureAllView()
+    }
+
+    private func addSubviews() {
+        contentView.addSubview(unitImageView)
+        contentView.addSubview(progressView)
+        contentView.addSubview(labelStackView)
+        labelStackView.addArrangedSubview(unitOriginalValue)
+        labelStackView.addArrangedSubview(unitPrimaryTranslation)
+    }
+
+    private func setAllConstraints() {
+        setUnitImageViewConstraints()
+        setProgressViewConstraints()
+        setLabelStackViewConstraints()
+    }
+
+    private func setUnitImageViewConstraints() {
+        unitImageView.snp.makeConstraints { make in
+            make.height.width.equalTo(UNIT_IMAGE_VIEW_EDGES)
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().inset(UNIT_IMAGE_LEFT_INSET)
+        }
+    }
+
+    private func setProgressViewConstraints() {
+        progressView.snp.makeConstraints { make in
+            make.height.width.equalTo(PROGRESS_VIEW_EDGE)
+            make.right.equalToSuperview().inset(PROGRESS_VIEW_INSET)
+            make.centerY.equalToSuperview()
+        }
+    }
+
+    private func setLabelStackViewConstraints() {
+        labelStackView.snp.makeConstraints { make in
+            make.left.equalTo(unitImageView.snp.left).inset(STACK_VIEW_MERGIN)
+            make.right.equalTo(progressView.snp.right).inset(STACK_VIEW_MERGIN)
+            make.top.bottom.equalToSuperview().inset(STACK_VIEW_MERGIN)
+        }
+    }
+
+    private func configureAllView() {
+        configureStackView()
+        configureUnitOriginalValue()
+        configureUnitPrimaryTranslation()
+    }
+
+    private func configureStackView() {
+        labelStackView.axis = .vertical
+        labelStackView.spacing = SPACING_BETWEEN_LABELS
+    }
+
+    private func configureUnitOriginalValue() {
+        //      Use R.swift
+        //      Use color strategy for colors and font sizes
+        unitOriginalValue.font = .systemFont(ofSize: 20)
+        unitOriginalValue.numberOfLines = 1
+        unitOriginalValue.textColor = .blue
+    }
+
+    private func configureUnitPrimaryTranslation() {
+        //      Use R.swift
+        //      Use color strategy for colors and font sizes
+        unitPrimaryTranslation.font = .systemFont(ofSize: 20)
+        unitPrimaryTranslation.numberOfLines = 1
+        unitPrimaryTranslation.textColor = .blue
+    }
 }
