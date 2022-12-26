7479e92d8a3ac7bb62bf9084b786c9e76a278db9
Dennis <dnsljnv411@gmail.com>
2022-12-26 14:12:38 +0700

    Remove redundant method

--- iWord/Screens/FolderListScreen/Subviews/FolderTableViewCell.swift
+++ iWord/Screens/FolderListScreen/Subviews/FolderTableViewCell.swift
@@ -54,7 +54,6 @@ extension FolderTableViewCell {
         addSubviews()
         setAllConstraints()
         configureAllView()
-        configureAllView()
     }
     
     private func addSubviews() {
