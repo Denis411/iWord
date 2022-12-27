0660eb08b40851e3691882753c7bc7b8e18814a6
Dennis <dnsljnv411@gmail.com>
2022-12-27 17:01:07 +0700

    Create files for creating of lexical units

---
+++ iWord/Screens/LexicalUnitCreationScreen/LexicalUnitCreationViewController.swift
@@ -0,0 +1,27 @@
+// Data of creation: 27/12/22
+// Create by Denis Ulianov aka Денис Александрович Ульянов
+// Using Swift 5.0
+// Running on macOS 12.5
+//
+// Copyrights
+//
+// I do not allow any person to use this piece of software
+// for any purposes. 
+// This project and source code may use libraries or frameworks that are
+// released under various Open-Source licenses. Use of those libraries and
+// frameworks are governed by their own individual licenses.
+
+import UIKit
+
+class LexicalUnitCreationViewController: UIViewController {
+    var mainView: LexicalUnitCreationView { view as! LexicalUnitCreationView }
+
+    override func loadView() {
+        view = LexicalUnitCreationView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
