ab18eeb4ef8bebaf7dc7068357ff4e8c27f7c3f3
Dennis <dnsljnv411@gmail.com>
2023-01-01 19:49:55 +0700

    Separate LexicalUnitExampleTableViewCell

---
+++ iWord/Screens/LexicalUnitCreationScreen/Subviews/LexicalUnitCreationView/LexicalUnitCreationExample/LexicalUnitExampleTableViewCell.swift
@@ -0,0 +1,79 @@
+// Data of creation: 1/1/23
+// Create by Denis Ulianov aka Денис Александрович Ульянов
+// Using Swift 5.0
+// Running on macOS 12.5
+//
+// Copyrights
+//
+// I do not allow any person to use this piece of software
+// for any purposes. 
+// This project and source code may use libraries or frameworks that are
+// released under various Open-Source licenses. Use of those libraries and
+// frameworks are governed by their own individual licenses.
+
+import UIKit
+
+final class LexicalUnitExampleTableViewCell: UITableViewCell {
+    static let reusableID = "LexicalUnitExampleTableViewCell"
+    let originTile = UILabel()
+    let translationTitle = UILabel()
+    
+    func setCellData(with example: LexicalUnitExample) {
+        self.originTile.text = example.original
+        self.translationTitle.text = example.translation
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension LexicalUnitExampleTableViewCell {
+    private func setUpUI() {
+        addAllSubviews()
+        addAllConstraints()
+    }
+    
+    private func addAllSubviews() {
+        contentView.addSubview(originTile)
+        contentView.addSubview(translationTitle)
+    }
+    
+    private func addAllConstraints() {
+        addOriginTitleConstraints()
+        addTranslationTitleConstraints()
+    }
+
+    private func configureAllSubviews() {
+        configureOriginTitle()
+        configureTranslationTitle()
+    }
+    
+    private func addOriginTitleConstraints() {
+        originTile.snp.makeConstraints { make in
+            make.top.left.right.equalToSuperview()
+        }
+    }
+    
+    private func addTranslationTitleConstraints() {
+        translationTitle.snp.makeConstraints { make in
+            make.top.equalTo(originTile.snp.bottom)
+            make.left.right.bottom.equalToSuperview()
+        }
+    }
+    
+    private func configureOriginTitle() {
+        originTile.backgroundColor = .white
+        originTile.numberOfLines = 0
+    }
+    
+    private func configureTranslationTitle() {
+        translationTitle.backgroundColor = .gray
+        translationTitle.numberOfLines = 0
+    }
+}
