d52e651389263b8bf331a51cd3b91ab93aa51d3c
Dennis <dnsljnv411@gmail.com>
2023-01-01 19:48:24 +0700

    Add LexicalUnitCreationExampleView

---
+++ iWord/Screens/LexicalUnitCreationScreen/Subviews/LexicalUnitCreationView/LexicalUnitCreationExampleView.swift
@@ -0,0 +1,211 @@
+// Data of creation: 1/1/23
+// Create by Denis Ulianov aka Денис Александрович Ульянов
+// Using Swift 5.0
+// Running on macOS 12.5
+//
+// Copyrights
+//
+// I do not allow any person to use this piece of software
+// for any purposes. 
+// This project and source code may use libraries or frameworks that are
+// released under various Open-Source licenses. Use of those libraries and
+// frameworks are governed by their own individual licenses.
+
+import UIKit
+
+final class LexicalUnitExampleTableViewCell: UITableViewCell {
+    static let reusableID = "LexicalUnitExampleTableViewCell"
+    let originTile = UILabel()
+    let translationTitle = UILabel()
+
+    func setCellData(with example: LexicalUnitExample) {
+        self.originTile.text = example.original
+        self.translationTitle.text = example.translation
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension LexicalUnitExampleTableViewCell {
+    private func setUpUI() {
+        addAllSubviews()
+        addAllConstraints()
+    }
+
+    private func addAllSubviews() {
+        contentView.addSubview(originTile)
+        contentView.addSubview(translationTitle)
+    }
+
+    private func addAllConstraints() {
+        addOriginTitleConstraints()
+        addTranslationTitleConstraints()
+    }
+
+    private func addOriginTitleConstraints() {
+        originTile.snp.makeConstraints { make in
+            make.top.left.right.equalToSuperview()
+        }
+    }
+
+    private func addTranslationTitleConstraints() {
+        translationTitle.snp.makeConstraints { make in
+            make.top.equalTo(originTile.snp.bottom)
+            make.left.right.bottom.equalToSuperview()
+        }
+    }
+
+    private func configureAllSubviews() {
+        configureOriginTitle()
+    }
+
+    private func configureOriginTitle() {
+        originTile.backgroundColor = .white
+        originTile.numberOfLines = 0
+    }
+
+    private func configureTranslationTitle() {
+        translationTitle.backgroundColor = .gray
+        translationTitle.numberOfLines = 0
+    }
+}
+
+
+typealias LexicalUnitExample = (original: String, translation: String)
+
+let GAP_BETWEEN_TITLE_AND_TABLEVIEW: CGFloat = 10.0
+
+final class LexicalUnitCreationExampleView: CommonView {
+    private let title = UILabel()
+    private let addExampleButton = UIButton()
+    private let tableView = UITableView()
+    private var heightConstraint: NSLayoutConstraint?
+
+    private var examples: [LexicalUnitExample] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+
+    private var onAddExampleAction: EmptyClosure?
+
+    override func setUpUI() {
+        addAllSubviews()
+        addAllConstraints()
+        configureAllSubviews()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.heightConstraint?.constant = title.bounds.height + GAP_BETWEEN_TITLE_AND_TABLEVIEW + tableView.contentSize.height
+    }
+
+    func setOnAddExampleActions(_ action: @escaping EmptyClosure) {
+        self.onAddExampleAction = action
+    }
+
+    func addArrayOfExamples(_ examples: [LexicalUnitExample]) {
+        self.examples.append(contentsOf: examples)
+    }
+
+    func addExample(_ example: LexicalUnitExample) {
+        self.examples.append(example)
+    }
+
+    func removeExample(at index: Int) {
+        self.examples.remove(at: index)
+    }
+}
+
+extension LexicalUnitCreationExampleView {
+    private func addAllSubviews() {
+        self.addSubview(addExampleButton)
+        self.addSubview(title)
+        self.addSubview(tableView)
+    }
+
+    private func addAllConstraints() {
+        addExampleButtonConstraints()
+        addTitleConstraints()
+        addTableViewConstraints()
+        addHeightConstraint()
+    }
+
+    private func addExampleButtonConstraints() {
+        addExampleButton.snp.makeConstraints { make in
+            make.top.right.equalToSuperview()
+            make.edges.equalTo(40)
+        }
+    }
+
+    private func addTitleConstraints() {
+        title.snp.makeConstraints { make in
+            make.top.left.equalToSuperview()
+            make.right.equalTo(addExampleButton.snp.left)
+        }
+    }
+
+    private func addTableViewConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(title.snp.bottom).offset(GAP_BETWEEN_TITLE_AND_TABLEVIEW)
+            make.left.right.bottom.equalToSuperview()
+        }
+    }
+
+    private func addHeightConstraint() {
+        let selfHeight = title.bounds.height + GAP_BETWEEN_TITLE_AND_TABLEVIEW + tableView.contentSize.height
+        self.heightConstraint = self.heightAnchor.constraint(equalToConstant: selfHeight)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.heightConstraint?.priority = UILayoutPriority(1000)
+        self.heightConstraint?.isActive = true
+    }
+
+    private func configureAllSubviews() {
+        configureAddExampleButton()
+        configureTitle()
+        configureTableView()
+    }
+
+    private func configureAddExampleButton() {
+        addExampleButton.backgroundColor = .blue
+        addExampleButton.addTarget(self, action: #selector(performOnAddExampleAction), for: .touchUpInside)
+    }
+
+    private func configureTitle() {
+        title.font = .systemFont(ofSize: 20)
+    }
+
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(LexicalUnitExampleTableViewCell.self, forCellReuseIdentifier: LexicalUnitExampleTableViewCell.reusableID)
+    }
+}
+
+// MARK: - Selectors -
+extension LexicalUnitCreationExampleView {
+    @objc private func performOnAddExampleAction() {
+        onAddExampleAction?()
+    }
+}
+
+// MARK: - UITableViewDataSource, UITableViewDelegate -
+extension LexicalUnitCreationExampleView: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        examples.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = LexicalUnitExampleTableViewCell()
+        cell.setCellData(with: examples[indexPath.row])
+        return cell
+    }
+}
+
